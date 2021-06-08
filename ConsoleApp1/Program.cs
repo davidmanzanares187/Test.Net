8d6a4df0f7fa8748fc301e0a6df4dac13d7a7c23
davidmanzanares187 <davidmanzanares187@hotmail.com>
2021-06-08 08:10:28 +0200

    Commit1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -9,14 +10,16 @@ namespace ConsoleApp1
             Console.WriteLine("Comenzando Proceso");
             try
             {
-                Solution sol = new Solution();
-                int[] A = new int[] { 1, 6, 4, 5, 1, 2, 3, 4, 7, 2 };                
-                int cantidadDePicos = sol.solution(A);
-                Console.WriteLine("Cantidad de picos: " + cantidadDePicos);
+                CortarString();
+                //Solution sol = new Solution();
+                //int[] A = new int[] { 1, 6, 4, 5, 1, 2, 3, 4, 7, 2 };
+                ////int[] A = new int [0] ;
+                //int cantidadDePicos = sol.solution(A);
+                //Console.WriteLine("Cantidad de picos: " + cantidadDePicos);
 
-                int[] B = new int[] { 1, 6, 4, 5, 4, 5, 1, 2, 3, 4, 7, 2 };
-                int cantidadDePicos2 = sol.solution(B);
-                Console.WriteLine("Cantidad de picos: " + cantidadDePicos2);
+                //int[] B = new int[] { 1, 6, 4, 5, 4, 5, 1, 2, 3, 4, 7, 2 };
+                //int cantidadDePicos2 = sol.solution(B);
+                //Console.WriteLine("Cantidad de picos: " + cantidadDePicos2);
                 Console.WriteLine("Proceso Ok");
                 Console.ReadLine();
             }
@@ -24,6 +27,21 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("Error: " + ex.Message);
             }            
+        }
+
+        private static void CortarString()
+        {
+            StringBuilder cortar = new StringBuilder();
+            cortar.Append("12345678901");
+            Console.WriteLine("Intenta extraer 50");
+            Console.ReadLine();
+            Console.WriteLine(cortar.ToString().Substring(0, 10));
+            Console.WriteLine(cortar.ToString().Substring(0, 10));
+            Console.ReadLine();
+            //cortar.Length = 5;
+            Console.WriteLine(cortar.ToString());
+            Console.WriteLine("cantidad" + cortar.Length.ToString());
+            Console.ReadLine();
         }
     }
 }
